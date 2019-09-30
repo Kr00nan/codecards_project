2a8ce37df361d26f523f780ef9287a95bd420f05
Audrey Garcia <audrey.marcella9@gmail.com>
2019-09-30 17:20:39 -0600

    create basic card controller

---
+++ app/controllers/api/cards_controller.rb
@@ -0,0 +1,47 @@
+class Api::CardsController < ApplicationController
+  before_action :set_deck
+  before_action :set_card, only: [:show, :update, :destroy]
+  
+  def index
+    render json: @deck.cards.all
+  end
+
+  def show
+    render json: @card
+  end
+
+  def create
+    card = @deck.cards.new(card_params)
+    if card.save
+      render json: card
+    else
+      render json: card.errors, status: 422
+    end
+  end
+
+  def update
+    if @card.update(card_params)
+      render json: @card
+    else
+      render json: @card.errors, status: 422
+    end
+  end
+
+  def destroy
+    @card.destroy
+  end
+
+  private
+    def set_deck
+      @deck = Deck.find(params[:deck_id])
+    end
+
+    def card_params
+      params.require(:cards).permit(:question, :answer)
+    end
+
+    def set_card
+      @card = Card.find(params[:id])
+    end
+
+end
