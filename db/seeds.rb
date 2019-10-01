20ce22fda30926225eb736220b2a8fbbcfe62a9b
Michael Kroon <kroonology@gmail.com>
2019-10-01 12:17:54 -0600

    improve seed file to include decks and cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,14 @@ User.create( email: 'kroon@test.com', password: 'password', admin: true)
 User.create( email: 'brown@test.com', password: 'password', admin: true)
 User.create( email: 'Gish@test.com', password: 'password', admin: true)
 User.create( email: 'Garcia@test.com', password: 'password', admin: true)
+
+5.times do
+d = Deck.create(title: Faker::Book.title, public: false, user_id: rand(1..5))
+  10.times do
+    d.cards.create(
+      question: Faker::Lorem.paragraph,
+      answer: Faker::Lorem.sentence
+  )
+end
+end
+
