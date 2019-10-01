fef77386a59314420faea985614266f7fd1ec60d
Devyn Gish <devyngish@gmail.com>
2019-10-01 16:46:02 -0600

    flipping the card works

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -3,7 +3,14 @@ import { Header, } from 'semantic-ui-react';
 import axios from 'axios'
 import { Button, Card, Image, Container} from 'semantic-ui-react'
 import { Link } from 'react-router-dom' 
-import CardImage from "../images/Ruby_A1.png"
+import CardImageA1 from "../images/Ruby_A1.png"
+import CardImageQ1 from "../images/Ruby_Q1.png"
+// import CardImageA2 from "../images/Ruby_A2.png"
+// import CardImageQ2 from "../images/Ruby_Q2.png"
+// import CardImageA3 from "../images/Ruby_A3.png"
+// import CardImageQ3 from "../images/Ruby_Q3.png"
+// import CardImageA4 from "../images/Ruby_A4.png"
+// import CardImageQ4 from "../images/Ruby_Q4.png"
 import Flippy, { FrontSide, BackSide } from 'react-flippy'
 
 class Deck extends React.Component  {
@@ -34,15 +41,20 @@ class Deck extends React.Component  {
             // if you pass isFlipped prop component will be controlled component.
             // and other props, which will go to div
             style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
-  >          <Container>
-            <FrontSide
-               style={{
-               backgroundColor: '#41669d',
-               }} > Hello
-               </FrontSide>
-                <Image src={CardImage} width='200px' length='200px' />
-             </Container>
+  >             <Container>
+                  <FrontSide
+                     style={{
+                     backgroundColor: '#41669d',
+                     }} ><Image src={CardImageQ1} width='200px' length='200px' />
+                  </FrontSide>
+                   <BackSide
+                      style={{ backgroundColor: '#41669d'}}>
+                      <Image src={CardImageA1} width='200px' length='200px'></Image>
+                   </BackSide>
+                </Container>
       </Flippy>
+
+      
 
 
    //   <Card>
