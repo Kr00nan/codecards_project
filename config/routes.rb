7ebf1c7db00a5c50fc100031755e68d77e699e72
Michael Kroon <kroonology@gmail.com>
2019-10-01 11:43:02 -0600

    deleted :user :deck route from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   
   namespace :api do
-    resources :users do
-      resources :decks
-    end
-
     resources :decks do
       resources :cards
     end
