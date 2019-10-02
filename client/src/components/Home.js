f7f34a3bc6804c9a9ef73762c7d23ac16c35d02d
Levi Brown <rlevibrown@gmail.com>
2019-10-02 14:35:39 -0600

    Add containers to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import { Header, } from 'semantic-ui-react';
+import { Container, Divider, Grid, Header, Image, } from 'semantic-ui-react';
 
 const Home = () => (
-  <Header as="h3" textAlign="center">Devise Auth App</Header>
-)
+  <>
+    <Container>
+      <Header as="h3" color="violet"textAlign="center">Dev Point Labs Code Card</Header>
+      <Grid>
+      <Grid.Column mobile={16} tablet={8} computer={5}>
+        <Header>HTML</Header>
+      </Grid.Column>   
+      <Grid.Column mobile={16} tablet={8} computer={5}>
+        <Header>CSS</Header>
+      </Grid.Column>
+      <Grid.Column mobile={16} tablet={8} computer={5}>
+        <Header>REACT</Header>
+      </Grid.Column>
+      <Grid.Column mobile={16} tablet={8} computer={5}>
+        <Header>RUBY</Header>
+      </Grid.Column>
+      </Grid>
+    </Container>
+  </>
+);
+
+
 
 export default Home;
