62ba394099ac988dbd9eb20ae7455c97aabed8b0
Audrey Garcia <audrey.marcella9@gmail.com>
2019-10-02 16:58:28 -0600

    change deck component and remove flashcards

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -1,100 +1,50 @@
 import React from 'react';
-import { Header, } from 'semantic-ui-react';
 import axios from 'axios'
-import { Button, Card, Image, Container} from 'semantic-ui-react'
-import { Link } from 'react-router-dom' 
-import CardImage from "../images/Ruby_A1.png"
-import Flippy, { FrontSide, BackSide } from 'react-flippy'
+import { Header, Card, } from 'semantic-ui-react'
+import { Link, } from 'react-router-dom'
 
 class Deck extends React.Component  {
-  state = { decks:[] }
+  state = { deck: {}, cards: [], }
 
   componentDidMount() {
-    axios.get('api/decks')
-    .then(res => {this.setState({decks:res.data})})
+    const { id, } = this.props.match.params
+    axios.get(`/api/decks/${id}`)
+      .then(res => {
+        this.setState({ deck: res.data, })
+      })
 
+    axios.get(`/api/decks/${id}/cards`)
+      .then( res => {
+        this.setState({ cards: res.data, })
+      })
+      .catch( err => {
+        console.log(err)
+      })
   }
  
-  
+  render() {
+    const { deck, } = this.state
+    return(
+      <>
+        <Header as="h1" textAlign="center">
+          {deck.title}
+        </Header>
+        <Card.Group itemsPerRow={4}>
+          {
+            this.state.cards.map( card =>
+              <Card 
+                key={card.id} 
+                color="grey" 
+                image={card.question} 
+                as={Link}
+                to="/flashcard" // route subject to change
+              />
+            )
+          }
+        </Card.Group>
+      </>
+    )
+  }
+}
 
-// OnClick () {
-// axios
-
-
-
-  render() {return(
-
-
-    
-         <Flippy
-            flipOnHover={false} // default false
-            flipOnClick={true} // default false
-            flipDirection="horizontal" // horizontal or vertical
-            ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
-            // if you pass isFlipped prop component will be controlled component.
-            // and other props, which will go to div
-            style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
-  >          <Container>
-            <FrontSide
-               style={{
-               backgroundColor: '#41669d',
-               }} > Hello
-               </FrontSide>
-                <Image src={CardImage} width='200px' length='200px' />
-             </Container>
-      </Flippy>
-
-
-   //   <Card>
-   //    {this.state.decks.map(deck => 
-      
-   //       <Card.Content>
-   //             <Card.Header>
-   //                <Link to="/CSS">
-   //                    {deck.title}
-   //                </Link>
-   //             </Card.Header>
-   //          </Card.Content>
-   //       )}
-  
-         /* <Card.Content>
-          <Card.Header>
-           <Link to="/CSS">
-              
-           </Link>
-          </Card.Header>
-         </Card.Content>
-         
-
-
-         <Card.Content>
-          <Card.Header>
-           <Link to="/CSS">
-            
-           </Link>
-          </Card.Header>
-         </Card.Content>
-
- 
-         <Card.Content>
-          <Card.Header>
-           <Link to="/CSS">
-            
-           </Link>
-          </Card.Header>
-         </Card.Content>
- 
-         <Card.Content>
-          <Card.Header>
-           <Link to="/CSS">
-         
-           </Link>
-          </Card.Header>
-         </Card.Content>
-
-    </Card> */
-  
-       )
-     }
-   }
 export default Deck
