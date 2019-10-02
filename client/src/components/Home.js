aa655a9a80dee31939b97cbf51a09969bdedc60c
Levi Brown <rlevibrown@gmail.com>
2019-10-02 16:00:53 -0600

    Add corrisponding boxes to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,62 @@
 import React from 'react';
-import { Container, Divider, Grid, Header, Image, } from 'semantic-ui-react';
+import { Container, Divider, Grid, Header, Card, } from 'semantic-ui-react';
 
 const Home = () => (
   <>
     <Container>
       <Header as="h3" color="violet"textAlign="center">Dev Point Labs Code Card</Header>
-      <Grid>
-      <Grid.Column mobile={16} tablet={8} computer={5}>
-        <Header>HTML</Header>
-      </Grid.Column>   
-      <Grid.Column mobile={16} tablet={8} computer={5}>
-        <Header>CSS</Header>
-      </Grid.Column>
-      <Grid.Column mobile={16} tablet={8} computer={5}>
-        <Header>REACT</Header>
-      </Grid.Column>
-      <Grid.Column mobile={16} tablet={8} computer={5}>
-        <Header>RUBY</Header>
-      </Grid.Column>
+      <br />
+      <br />
+      <br />
+      <Grid centered columns='4' >
+          <Grid divided vertically>
+              <Grid.Column  mobile={4} tablet={8} computer={6}></Grid.Column>
+                <div class="ui fluid card">
+                  <div class="image"></div>
+                    <div class="content">
+                      <div class="header">HTML</div>
+                        <div class="ui bottom attached button">
+                          START
+                        </div>
+                      </div>
+                    </div>
+              <br />
+              <br />
+              <Grid.Column  mobile={4} tablet={8} computer={6}></Grid.Column>
+              <div class="ui fluid card">
+                <div class="image"></div>
+                  <div class="content">
+                    <div class="header">JS</div>
+                      <div class="ui bottom attached button">
+                        START
+                      </div>
+                    </div>
+                  </div>
+              <br />
+              <br />
+              <Grid.Column  mobile={4} tablet={8} computer={6}></Grid.Column>
+              <div class="ui fluid card">
+                <div class="image"></div>
+                  <div class="content">
+                    <div class="header">REACT</div>
+                      <div class="ui bottom attached button">
+                        START
+                      </div>
+                    </div>
+                  </div>
+              <br />
+              <br />
+              <Grid.Column  mobile={4} tablet={8} computer={6}></Grid.Column>
+              <div class="ui fluid card">
+                <div class="image"></div>
+                  <div class="content">
+                    <div class="header">RUBY</div>
+                      <div class="ui bottom attached button">
+                        START
+                      </div>
+                    </div>
+                  </div>
+          </Grid>
       </Grid>
     </Container>
   </>
