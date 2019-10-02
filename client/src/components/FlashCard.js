4b9dcfbe37cae84325f627fa5134e754073b78fe
Audrey Garcia <audrey.marcella9@gmail.com>
2019-10-02 14:24:49 -0600

    move card from flashcard.js to flachcards.js

--- client/src/components/FlashCard.js
+++ client/src/components/FlashCard.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Card, } from 'semantic-ui-react';
 
 class FlashCard extends React.Component {
   
   render() {
     return (
-      <>
-        <Card color="yellow" image={this.props.question} />
-        <Card color="yellow" image={this.props.answer} />        
+      <>    
       </>
     );
   };
