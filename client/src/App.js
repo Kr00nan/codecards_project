2f7d16f9062b71cd325eae5e239f9e39a53daf0e
Audrey Garcia <audrey.marcella9@gmail.com>
2019-10-03 15:21:33 -0600

    keep all new routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,9 @@ import Register from './components/Register';
 import FetchUser from './components/FetchUser';
 import ProtectedRoute from './components/ProtectedRoute';
 import Deck from './components/Deck';
-<<<<<<< HEAD
+import FlashCard from './components/FlashCard';
 import Profile from './components/Profile'
 import User from './components/User'
-=======
-import FlashCard from './components/FlashCard';
->>>>>>> de32ffaed3fb60e4004c1b7a4cde75761de174aa
 import { Switch, Route, } from 'react-router-dom';
 import { Container, } from "semantic-ui-react";
 
@@ -26,12 +23,9 @@ const App = () => (
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/decks/:id" component={Deck} />
-<<<<<<< HEAD
+          <Route exact path="/decks/:deck_id/cards/:id" component={FlashCard} />
           <ProtectedRoute exact path="/my_profile" component={Profile} />
           <ProtectedRoute exact path="/users/:id" component={User} />
-=======
-          <Route exact path="/decks/:deck_id/cards/:id" component={FlashCard} />
->>>>>>> de32ffaed3fb60e4004c1b7a4cde75761de174aa
           <Route component={NoMatch} />
         </Switch>
       </Container>
