2b1a47df5d2d967ede76d16ac791c0b05effc141
Michael Kroon <kroonology@gmail.com>
2019-10-07 15:20:37 -0600

    add prev and next arrows for flashcard navigation

--- client/src/components/FlashCard.js
+++ client/src/components/FlashCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Image, } from 'semantic-ui-react';
+import { Icon, } from 'semantic-ui-react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
-
-
 import axios from 'axios';
-
 class FlashCard extends React.Component {
   state = { card: {}, }
-
   componentDidMount() {
+    this.getCard();
+  }
+  componentDidUpdate() {
+    if ( this.props.match.params.id != this.state.card.id ) {
+      this.getCard();
+    }
+  }
+  getCard = () => {
     const {deck_id, id} = this.props.match.params;
     axios.get(`/api/decks/${deck_id}/cards/${id}`)
       .then(res => {
@@ -19,34 +23,56 @@ class FlashCard extends React.Component {
         console.log(err)
       })
   }
-
-
   render() {
-    const { question, answer, extra, } = this.state.card
+    const { id, question, answer, extra, deck_id } = this.state.card
     return (
       <>
-        <Link to='/decks/1'>Back to deck</Link>
+        <Link to={`/decks/${deck_id}`}>Back to deck</Link>
         <br />
         <br />
+        <Link to={`/decks/${deck_id}/cards/${id - 1}`}>
+          <Icon name="arrow left" size="huge" style={styles.left} />
+        </Link>
         <Flippy
           flipOnClick={true}
           flipDirection="horizontal"
           ref={(r) => this.flippy = r}
-          style={{ width: '500px', borderRadius: '5px' }}
         >
-          <FrontSide>
+          <FrontSide style={styles.card}>
             {question}
           </FrontSide>
-          <BackSide>
+          <BackSide style={styles.card}>
             {answer}
             <br />
             <br />
-            {extra}
+            <div style={{fontSize: '18px',}}>{extra}</div>
           </BackSide>
         </Flippy>
+        <Link to={`/decks/${deck_id}/cards/${id + 1}`}>
+          <Icon name="arrow right" size="huge" style={styles.right} />
+        </Link>
       </>
     );
   };
 };
-
+const styles = {
+  left: {
+    position: "fixed",
+    left: "0",
+    top: "50%",
+  },
+  right: {
+    position: 'fixed',
+    right: '0',
+    top: '50%'
+  },
+  card: {
+    padding: '25px', 
+    borderRadius: '25px', 
+    width: '400px',
+    height: '500px',
+    fontSize: '25px',
+    lineHeight: 'normal',
+  },
+}
 export default FlashCard;
