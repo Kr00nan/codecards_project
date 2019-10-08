9314f7bfd7276913e303762a7619a392fe960732
Michael Kroon <kroonology@gmail.com>
2019-10-08 12:25:41 -0600

    change cards to card in card_params method

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -37,7 +37,7 @@ class Api::CardsController < ApplicationController
     end
 
     def card_params
-      params.require(:cards).permit(:question, :answer)
+      params.require(:card).permit(:question, :answer)
     end
 
     def set_card
