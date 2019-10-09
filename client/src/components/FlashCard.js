fddd9d56b9fbea0e399ee2eb4778809651a142e9
Devyn Gish <devyngish@gmail.com>
2019-10-09 16:48:41 -0600

    edit_card

--- client/src/components/FlashCard.js
+++ client/src/components/FlashCard.js
@@ -1,40 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Icon, Button, } from 'semantic-ui-react';
+import { Icon, Button, Form } from 'semantic-ui-react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import axios from 'axios';
 
+
 class FlashCard extends React.Component {
-  state = { card: {}, }
-  
-  
+  state = { card: {}, showForm: false, question: "", }
+
+
   componentDidMount() {
+
     this.getCard();
   }
   componentDidUpdate() {
-    if ( this.props.match.params.id != this.state.card.id ) {
+    if (this.props.match.params.id != this.state.card.id) {
       this.getCard();
     }
   }
 
+  toggleShowForm = () => this.setState({ showForm: !this.state.showForm, })
+
   handleDelete = () => {
-    const {deck_id, id} = this.props.match.params;
+    const { deck_id, id } = this.props.match.params;
     axios.delete(`/api/decks/${deck_id}/cards/${id}`)
-      .then( this.props.history.push(`/decks/${deck_id}`) )
+      .then(this.props.history.push(`/decks/${deck_id}`))
   }
 
   getCard = () => {
-    const {deck_id, id} = this.props.match.params;
+    const { deck_id, id } = this.props.match.params;
     axios.get(`/api/decks/${deck_id}/cards/${id}`)
       .then(res => {
-        this.setState({ card: res.data, })
+        this.setState({ card: res.data, question: res.data.question })
       })
       .catch(err => {
         console.log(err)
+      })  
+  }
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { card: {deck_id, id,}, question} = this.state;
+    axios.put(`/api/decks/${deck_id}/cards/${id}`, { question })
+      .then(res => {
+        this.setState({ card: res.data, })
+        this.toggleShowForm()
+      })
+      .catch(err => {
+
       })
   }
+
   render() {
-    const { id, question, answer, extra, deck_id } = this.state.card
+    const { id, question, answer, showForm, extra, deck_id } = this.state.card
     return (
       <>
         <Link to={`/decks/${deck_id}`}>Back to deck</Link>
@@ -56,12 +78,35 @@ class FlashCard extends React.Component {
             {answer}
             <br />
             <br />
-            <div style={{fontSize: '18px',}}>{extra}</div>
+            <div style={{ fontSize: '18px', }}>{extra}</div>
           </BackSide>
         </Flippy>
         <Link to={`/decks/${deck_id}/cards/${id + 1}`}>
           <Icon name="arrow right" size="huge" style={styles.right} />
         </Link>
+        <Button onClick={this.toggleShowForm}>
+          Edit Card
+        </Button>
+
+    
+        {this.state.showForm ?
+          (
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Input
+                label="Question"
+                name="question"
+                value={this.state.question}
+                onChange={this.handleChange}
+
+                />    
+                <Form.Button>Submit</Form.Button>
+            </Form>
+            
+          )
+          : 
+            ""
+          }
+      
       </>
     );
   };
@@ -78,8 +123,8 @@ const styles = {
     top: '50%'
   },
   card: {
-    padding: '25px', 
-    borderRadius: '25px', 
+    padding: '25px',
+    borderRadius: '25px',
     width: '400px',
     height: '500px',
     fontSize: '25px',
