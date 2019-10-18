02a9244f969bc64dacaf49b4b6c5cd069a96e4d6
Levi Brown <rlevibrown@gmail.com>
2019-10-18 13:29:07 -0600

    add search function to Browse.js

--- client/src/components/Browse.js
+++ client/src/components/Browse.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Menu, Card, } from 'semantic-ui-react'
-import Searchbar from './Searchbar'
+import { Menu, Card, Grid} from 'semantic-ui-react'
 import axios from 'axios'
 import { Link, } from 'react-router-dom'
 import Flippy, { FrontSide, BackSide, } from 'react-flippy'
+import _ from 'lodash';
 
 class Browse extends React.Component {
-  state = { activeItem: 'decks', decks: [], cards: [], }
+  state = { activeItem: 'decks', decks: [], cards: [], isLoading: false, value: "", results: [], search: "", }
 
   componentDidMount() {
     axios.get('/api/public_decks')
@@ -17,11 +17,61 @@ class Browse extends React.Component {
 
   handleItemClick = (e, {name}) => this.setState({ activeItem: name })
 
+  handleResultSelect = (_e, { result }) => this.setState({ value: result.title })
+
+  handleSearchChange = (_e, { value }) => {
+    this.setState({ isLoading: true, value })
+
+    setTimeout(() => {
+      const initialState = { isLoading: false, results: [], value: '' }
+      if (this.state.value.length < 1) return this.setState(initialState)
+
+      const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
+      const isMatch = (result) => re.test(result.title)
+
+      this.setState({
+        isLoading: false,
+        results: _.filter( isMatch),
+      })
+    }, 300)
+  }
+
+  updateSearch(event) {
+    this.setState({
+      search: event.target.value
+    });
+  }
+
+
   render() {
-    const { activeItem, decks, cards, } = this.state
+    let filteredCards = this.state.cards.filter(
+      (card)=>{
+        return card.question.indexOf(
+          this.state.search)
+        !== -1;
+      }
+    );
+    const { activeItem, decks, } = this.state
     return (
       <>
-        <Searchbar position='center' /> 
+      <br />
+      <br />
+      <Grid>
+      <input type="text"
+      placeholder="search..."
+      value={this.state.search}
+      onChange={this.updateSearch.bind(this)}
+      />
+        <Grid.Column width={6}>
+          {/* <Input type="text"
+            onChange={this.updateSearch.bind(this)}
+            results={results}
+            value={value}
+          /> */}
+        </Grid.Column>
+      </Grid>
+      <br />
+      <br />
         <Menu fluid widths={2}>
           <Menu.Item
             name='decks'
@@ -49,7 +99,7 @@ class Browse extends React.Component {
             )
           }
           {(activeItem === 'cards') &&
-            cards.map( card =>
+            filteredCards.map( card =>
               <Card 
                 key={card.id}
                 style={{margin: '12px'}}
